749dee29e2eeb5378f06609e62ede9c204d734f6
Moistlos-2603 <moritz.albach@t-online.de>
2023-12-20 10:53:40 +0100

    Framwork start

--- cooler_roboter/main_controler.py
+++ cooler_roboter/main_controler.py
@@ -1,6 +1,41 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import *
+from pybricks.parameters import Port
+
+
+
+
 class Main_Controler:
   initiated = False
-  def __init__(self) -> None:
+  ev3 = None
+  xAchse = None
+  yAchse = None
+  zAchse = None
+
+  pen_down = False
+  
+  x_cord = 0
+  y_cord = 0
+
+  def __init__(self, xAchse = Motor(Port.C), yAchse = Motor(Port.B), zAchse = Motor(Port.A)) -> None:
     global initiated
     Main_Controler.initiated = True 
-    
+    self.ev3 = EV3Brick()
+
+    self.xAchse = xAchse
+    self.yAchse = yAchse
+    self.zAchse = zAchse
+ 
+
+  def to(self, x, y):
+    new_x = x + self.x_cord
+    new_y = y + self.y_cord
+
+  def move_diagonal():
+    pass
+  def point():
+    pass 
+  def line():
+    pass 
+
+  
